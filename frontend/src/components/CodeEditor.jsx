c450ea73a6a5655e41e31e4f524d109f2d9143ae
Sajid Mehmood Tariq <sajidmehmoodtariq5@gmail.com>
2026-07-06 21:03:47 +0500

    Setting up YJS

--- frontend/src/components/CodeEditor.jsx
+++ frontend/src/components/CodeEditor.jsx
@@ -1,10 +1,31 @@
 import Editor from '@monaco-editor/react';
+import { MonacoBinding } from 'y-monaco';
+import { useRef, useMemo } from 'react';
+import * as Y from 'yjs';
+import { SocketIOProvider } from 'y-socket.io';
 
 const CodeEditor = () => {
+  const editorRef = useRef(null);
 
-  function handleEditorChange(value) {
-    console.log('here is the current model value:', value);
-  }
+  const ydoc = useMemo(() => new Y.Doc(), []);
+  const ytext = useMemo(() => ydoc.getText('monaco'), [ydoc]);
+
+  const handleEditorDidMount = (editor) => {
+    editorRef.current = editor;
+
+    const model = editor.getModel();
+    if (!model) {
+      return;
+    }
+
+    const provider = new SocketIOProvider('http://localhost:3000', 'monaco', ydoc, {autoConnect: true});
+    new MonacoBinding(
+      ytext,
+      model,
+      new Set([editor]),
+      provider.awareness
+    );
+  };
 
   return (
     <Editor
@@ -12,9 +33,9 @@ const CodeEditor = () => {
       defaultLanguage="python"
       defaultValue='print("Hello world")'
       theme="vs-dark"
-      onChange={handleEditorChange}
+      onMount={handleEditorDidMount}
     />
-  )
-}
+  );
+};
 
 export default CodeEditor
